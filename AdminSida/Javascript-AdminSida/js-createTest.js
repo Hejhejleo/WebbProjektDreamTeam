96125af6a7cf70d154cb20bb4388e77e8ccb8392
Jannike <Jannike@rymden.nu>
2016-04-28 09:10:33 +0200

    createTest, nu med css och javascript

---
+++ AdminSida/Javascript-AdminSida/js-createTest.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Jannike on 2016-04-27.
+ */
+
+var questionNumber = 1;
+
+$(document).ready(function () {
+    document.getElementById("createQuestionForm").style.visibility = "hidden";
+});
+
+function saveTestInfo() {
+    // validera att vg-gräns > g-gräns
+    var g = document.getElementById("gPercent").value;
+    var vg = document.getElementById("vgPercent").value;
+    if (vg <= g) {
+        alert("Gränsen för VG måste vara högre än för G.");
+    } else {
+        document.getElementById("createQuestionForm").style.visibility = "visible";
+        document.getElementById("questionId").innerHTML = "Fråga " + questionNumber;
+    }
+}
+function saveQuestion() {
+    // spara data
+    document.getElementById("questionId").innerHTML = "Fråga " + ++questionNumber;
+    document.getElementById("createQuestionForm").reset();
+}
