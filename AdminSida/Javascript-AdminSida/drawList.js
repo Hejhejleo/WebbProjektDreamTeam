2a42f5e6e6f691c1e6dd6665691d9d95a8e6d19e
Leonard Asp <Leoasp96@gmail.com>
2016-04-29 12:52:11 +0200

    Revert "Lite bugg hantering ch skapa klasser till användare"
    This reverts commit 09ee01b156911c8c01d2c12df64f11072dc61963.

--- AdminSida/Javascript-AdminSida/drawList.js
+++ AdminSida/Javascript-AdminSida/drawList.js
@@ -4,9 +4,9 @@ $("body").on('click',".reload-list",function () {
     $(".handle-item").remove();
     for (var i = 0; i < users.mail.length; i++) {
         if (i % 2 == 0) {
-            $li = $("<li class='grid-100 handle-item grey' id='" + users.mail[i]+"'><div class='grid-25 handle-div'>" + users.firstName[i] + "</div><div class='grid-25 handle-div'>" + users.lastName[i] + "</div><div class='grid-25 handle-div'>" + users.className[i] + "</div><div class='grid-25 handle-div'>" + users.mail[i] + "</div></li>");
+            $li = $("<li class='grid-100 handle-item grey' id='" + users.mail[i]+"'><div class='grid-33 handle-div'>" + users.firstName[i] + "</div><div class='grid-33 handle-div'>" + users.lastName[i] + "</div><div class='grid-33 handle-div'>" + users.mail[i] + "</div></li>");
         } else {
-            $li = $("<li class='grid-100 handle-item white' id='" + users.mail[i]+ "'><div class='grid-25 handle-div'>" + users.firstName[i] + "</div><div class='grid-25 handle-div'>" + users.lastName[i] + "</div><div class='grid-25 handle-div'>" + users.className[i] + "</div><div class='grid-25 handle-div'>" + users.mail[i] + "</div></li>");
+            $li = $("<li class='grid-100 handle-item white' id='" + users.mail[i]+ "'><div class='grid-33 handle-div'>" + users.firstName[i] + "</div><div class='grid-33 handle-div'>" + users.lastName[i] + "</div><div class='grid-33 handle-div'>" + users.mail[i] + "</div></li>");
         }
 
         $("#handleUl").append($li);
