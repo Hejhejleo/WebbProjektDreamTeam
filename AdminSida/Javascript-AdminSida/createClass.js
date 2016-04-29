09ee01b156911c8c01d2c12df64f11072dc61963
Leonard Asp <Leoasp96@gmail.com>
2016-04-29 12:50:18 +0200

    Lite bugg hantering ch skapa klasser till användare

--- AdminSida/Javascript-AdminSida/createClass.js
+++ AdminSida/Javascript-AdminSida/createClass.js
@@ -1,3 +1,35 @@
 /**
  * Created by LeoAsp on 2016-04-29.
  */
+$(document).ready(function () {
+
+    $("#createClass").click(function () {
+
+        var inputValue = $("#inputClass").val();
+        var classes = JSON.parse(localStorage.getItem("classes"));
+        var className = new Array();
+        var boolean = true;
+        if (classes == null) {
+            console.log("Classes Is Null");
+            var emptyClass = {"className": [["Ingen Klass"]]};
+            localStorage.setItem("classes", JSON.stringify(emptyClass));
+            var classes = JSON.parse(localStorage.getItem("classes"));
+        }
+        console.log("Create class");
+        console.log(classes.className.length);
+        for (var i = 0; i < classes.className.length; i++) {
+            console.log("Check if exists" + i);
+            if (inputValue == classes.className[i]) {
+                alert("A class with that name allready exist!");
+                boolean = false;
+            }
+        }
+        if (boolean) {
+            console.log("Push value");
+            className.push(inputValue);
+            classes.className.push(className);
+            localStorage.setItem("classes", JSON.stringify(classes));
+        }
+    });
+
+});
