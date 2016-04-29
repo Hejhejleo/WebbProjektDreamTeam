3d31b964e0d2053eb583360bc61568acbfa63094
Leonard Asp <Leoasp96@gmail.com>
2016-04-29 12:53:02 +0200

    Revert "Revert "Lite bugg hantering ch skapa klasser till användare""
    This reverts commit 2a42f5e6e6f691c1e6dd6665691d9d95a8e6d19e.

--- AdminSida/Javascript-AdminSida/editUser.js
+++ AdminSida/Javascript-AdminSida/editUser.js
@@ -2,34 +2,39 @@ $(document).ready(function () {
 
     var userindex;
     var users = JSON.parse(localStorage.getItem("users"));
+    var classes = JSON.parse(localStorage.getItem("classes"));
 
-    $(".reload-list").on('click',function () {
+    /*$(".reload-list").on('click', function () {
         var users = JSON.parse(localStorage.getItem("users"));
         $("#handleUl").empty();
         $(".handle-item").remove();
         for (var i = 0; i < users.mail.length; i++) {
             if (i % 2 == 0) {
-                $li = $("<li class='grid-100 handle-item grey' id='" + users.mail[i]+"'><div class='grid-33 handle-div'>" + users.firstName[i] + "</div><div class='grid-33 handle-div'>" + users.lastName[i] + "</div><div class='grid-33 handle-div'>" + users.mail[i] + "</div></li>");
+                $li = $("<li class='grid-100 handle-item grey' id='" + users.mail[i] + "'><div class='grid-20 handle-div'>" + users.firstName[i] + "</div><div class='grid-20 handle-div'>" + users.lastName[i] + "</div><div class='grid-20 handle-div'>" + users.className[i] + "</div><div class='grid-20 handle-div'>" + users.mail[i] + "</div></li>");
             } else {
-                $li = $("<li class='grid-100 handle-item white' id='" + users.mail[i]+ "'><div class='grid-33 handle-div'>" + users.firstName[i] + "</div><div class='grid-33 handle-div'>" + users.lastName[i] + "</div><div class='grid-33 handle-div'>" + users.mail[i] + "</div></li>");
+                $li = $("<li class='grid-100 handle-item white' id='" + users.mail[i] + "'><div class='grid-20 handle-div'>" + users.firstName[i] + "</div><div class='grid-20 handle-div'>" + users.lastName[i] + "</div><div class='grid-20 handle-div'>" + users.className[i] + "</div><div class='grid-20 handle-div'>" + users.mail[i] + "</div></li>");
             }
 
             $("#handleUl").append($li);
         }
-    });
-    /*for (var i=0;i<classes.length();i++){
-    $select=$("<option value='"+classes[i]+"'>"+classes[i]+"</option>")
-    }
-    $("#handleKlass").append($select);*/
+    });*/
 
-        $("#handleUl").on("click","li.handle-item", function(){
+    if (classes == null) {
+    } else {
+        for (var i = 0; i < classes.className.length; i++) {
+            $select = $("<option value='" + classes.className[i] + "'>" + classes.className[i] + "</option>")
+            $("#handleKlass").append($select);
+        }
+
+    }
+    $("#handleUl").on("click", "li.handle-item", function () {
         var id = $(this).attr('id');
         for (var i = 0; i < users.mail.length; i++) {
             console.log("for");
             if (users.mail[i] == id) {
-            userindex=i;
+                userindex = i;
                 $("#handleFirstname").val(users.firstName[userindex]);
-                console.log( $("#handleFirstname").val());
+                console.log($("#handleFirstname").val());
                 $("#handleLastname").val(users.lastName[userindex]);
                 $("#handleEmail").val(users.mail[userindex]);
                 $("#handlePassword").val(users.password[userindex]);
@@ -39,37 +44,37 @@ $(document).ready(function () {
         }
 
     });
-        $("#handleButton").click(function () {
-            if ( $("#handleEmail").val().indexOf('@') != -1) {
-                if ($("#handleEmail").val().indexOf('.') != -1) {
-                   var FNameA=[$("#handleFirstname").val()];
-                    users.firstName[userindex]=FNameA;
-                    console.log($("#handleFirstname").val());
+    $("#handleButton").click(function () {
+        if ($("#handleEmail").val().indexOf('@') != -1) {
+            if ($("#handleEmail").val().indexOf('.') != -1) {
+                var FNameA = [$("#handleFirstname").val()];
+                users.firstName[userindex] = FNameA;
+                console.log($("#handleFirstname").val());
 
-                    var LNameA=[$("#handleLastname").val()];
-                    users.lastName[userindex]=LNameA;
-                    console.log($("#handleLastname").val());
+                var LNameA = [$("#handleLastname").val()];
+                users.lastName[userindex] = LNameA;
+                console.log($("#handleLastname").val());
 
-                    var mailA=[$("#handleEmail").val()];
-                    users.mail[userindex]=mailA;
-                    console.log($("#handleEmail").val());
+                var mailA = [$("#handleEmail").val()];
+                users.mail[userindex] = mailA;
+                console.log($("#handleEmail").val());
 
-                    var passwordA=[$("#handlePassword").val()];
-                    users.password[userindex]=passwordA;
-                    console.log($("#handlePassword").val());
+                var passwordA = [$("#handlePassword").val()];
+                users.password[userindex] = passwordA;
+                console.log($("#handlePassword").val());
 
-                    var classnameA=[$("#handlePassword").val()];
-                    users.className[userindex]=classnameA;
-                    console.log($("#handleKlass").val());
+                var classnameA = [$("#handleKlass").val()];
+                users.className[userindex] = classnameA;
+                console.log($("#handleKlass").val());
 
-                    localStorage.setItem("users",JSON.stringify(users));
+                localStorage.setItem("users", JSON.stringify(users));
 
-                }else {
-                    alert("A valid emails need a '.'");
-                }
-            }else{
-            alert("A valid emails need a '@'");
+            } else {
+                alert("A valid emails need a '.'");
             }
+        } else {
+            alert("A valid emails need a '@'");
+        }
 
     });
 
