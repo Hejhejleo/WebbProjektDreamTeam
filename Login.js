a56d3e08033ab930ba1a2d5ac3217639978d1e6c
Leonard Asp <Leoasp96@gmail.com>
2016-05-04 09:54:48 +0200

    Login knappen funkar på login nu

--- Login.js
+++ Login.js
@@ -1,17 +1,17 @@
-var username="";
-var password="";
+var username = "";
+var password = "";
 var users;
 var test;
 var seesion;
-var correctLogin=false;
+var correctLogin = false;
 
 $(document).ready(function () {
     users = JSON.parse(localStorage.getItem("users"));
     test = JSON.parse(localStorage.getItem("testdata"));
-    sessionStorage.setItem("loggedInAs","");
+    sessionStorage.setItem("loggedInAs", "");
     seesion = sessionStorage.getItem("loggedInAs");
-    
-    if(users == null){
+
+    if (users == null) {
         var admin = {
             "firstName": [["admin"]],
             "lastName": [["admin"]],
@@ -19,19 +19,19 @@ $(document).ready(function () {
             "mail": [["admin@admin.se"]],
             "password": [["password"]]
         };
-        localStorage.setItem("users",JSON.stringify(admin));
+        localStorage.setItem("users", JSON.stringify(admin));
         users = JSON.parse(localStorage.getItem("users"));
     }
 
-    if(test == null) {
+    if (test == null) {
         var obj = {
             "testName": [["test1"]],
             "testTime": [[40]],
             "questionString": [[["huvudstad?"], ["Städer?"]]],
             "answerType": [[["singleChoice"], ["multiChoice"]]],
-            "points": [[1],[2]],
+            "points": [[1], [2]],
             "answers": [[["Stockholm", "Göteborg", "Malmö", "Luleå"], ["Göteborg", "Stenköping", "London"]]],
-            "correctAnswers": [["Stockholm"],[["Göteborg"],["London"]]],
+            "correctAnswers": [["Stockholm"], [["Göteborg"], ["London"]]],
             "gProcent": [[60]],
             "vgProcent": [[80]],
             "autoCorrect": [[1]]
@@ -42,10 +42,10 @@ $(document).ready(function () {
 
     userTest = JSON.parse(localStorage.getItem("usertest"));
 
-    if(userTest == null){
+    if (userTest == null) {
         var ut = {
             "testName": [["test1"]],
-            "mail": [["r@r.com","leoasp96@gmail.com"]]
+            "mail": [["r@r.com", "leoasp96@gmail.com"]]
         };
         var objdata = JSON.stringify(ut);
         localStorage.setItem("usertest", objdata);
@@ -53,7 +53,7 @@ $(document).ready(function () {
 
     classes = JSON.parse(localStorage.getItem("classes"));
 
-    if(classes == null){
+    if (classes == null) {
         var cl = {
             "className": [["Ingen klass"]]
 
@@ -63,6 +63,17 @@ $(document).ready(function () {
     }
 
 
+    $("#login-button").on("click",function () {
+        startLogin();
+    });
+
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            startLogin();
+        }
+    });
+
+
 });
 
 function login(username, password) {
@@ -70,9 +81,9 @@ function login(username, password) {
     for (var i = 0; i < users.mail.length; i++) {
         if (users.mail[i] == username) {
             if (users.password[i] == password) {
-                correctLogin=true;
+                correctLogin = true;
                 saveUser(users.mail[i]);
-                if (username=="admin@admin.se") {
+                if (username == "admin@admin.se") {
                     location.href = "AdminSida/AdminSida.html"
                 } else {
                     location.href = "StudentSida/StudentLoginPage.html";
@@ -83,22 +94,9 @@ function login(username, password) {
     }
 }
 
-$(document).keypress(function (e) {
-    if (e.which == 13) {
-        users = JSON.parse(localStorage.getItem("users"));
-        username = $("#username").val().toLowerCase();
-        password = $("#password").val();
-        $("#password").val("")
-        login(username, password);
-        if(!correctLogin){
-            alert("Wrong email/password");
-        }
-    }
-
-});
 
 function saveUser(user) {
-    sessionStorage.setItem("loggedInAs",user);
+    sessionStorage.setItem("loggedInAs", user);
 }
 
 $(document).ready(function () {
@@ -107,11 +105,20 @@ $(document).ready(function () {
             location.href = "AdminSida/AdminSida.html"
         }
     }
-        if (!(seesion == "")) {
-            location.href = "StudentSida/StudentLoginPage.html";
-            alert(seesion);
-        }
-
-
+    if (!(seesion == "")) {
+        location.href = "StudentSida/StudentLoginPage.html";
+        alert(seesion);
+    }
 
 });
+
+function startLogin() {
+    users = JSON.parse(localStorage.getItem("users"));
+    username = $("#username").val().toLowerCase();
+    password = $("#password").val();
+    $("#password").val("")
+    login(username, password);
+    if (!correctLogin) {
+        alert("Wrong email/password");
+    }
+}
