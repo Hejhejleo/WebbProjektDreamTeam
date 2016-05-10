f3cebf1d3ef76c475a5f8d0170ebdec41bb20361
rezamars <reza.mars@yahoo.com>
2016-05-10 09:40:49 +0200

    la till kod för statistiken

--- AdminSida/Javascript-AdminSida/statistik.js
+++ AdminSida/Javascript-AdminSida/statistik.js
@@ -52,5 +52,83 @@ function getStats() {
     var max = document.getElementById('maxPoints');
     max.innerHTML = totalTestPoints;
 
+    var numberOfIGs = 0;
+    for(var i = 0 ; i<correctedTest.studentGrade.length ; i++) {
+        if (correctedTest.testName[i] == chosenTest) {
+
+            if (correctedTest.studentGrade[i] == 'IG') {
+                numberOfIGs++;
+            }
+        }
+    }
+
+    var IGlabel = document.getElementById('IGs');
+    IGlabel.innerHTML = numberOfIGs;
+
+    var percentIG = 0;
+    percentIG = (numberOfIGs * 100) / numberOfTestsDone;
+    var IGpercentLabel = document.getElementById('procentIG');
+    IGpercentLabel.innerHTML = percentIG.toFixed(2) + '%';
+
+    var numberOfGs = 0;
+    for(var i = 0 ; i<correctedTest.studentGrade.length ; i++) {
+        if (correctedTest.testName[i] == chosenTest) {
+
+            if (correctedTest.studentGrade[i] == 'G') {
+                numberOfGs++;
+            }
+        }
+    }
+
+    var Glabel = document.getElementById('Gs');
+    Glabel.innerHTML = numberOfGs;
+
+    var percentG = 0;
+    percentG = (numberOfGs * 100) / numberOfTestsDone;
+    var GpercentLabel = document.getElementById('procentG');
+    GpercentLabel.innerHTML = percentG.toFixed(2) + '%';
+
+    var numberOfVGs = 0;
+    for(var i = 0 ; i<correctedTest.studentGrade.length ; i++) {
+        if (correctedTest.testName[i] == chosenTest) {
+
+            if (correctedTest.studentGrade[i] == 'VG') {
+                numberOfVGs++;
+            }
+        }
+    }
+
+    var VGlabel = document.getElementById('VGs');
+    VGlabel.innerHTML = numberOfVGs;
+
+    var percentVG = 0;
+    percentVG = (numberOfVGs * 100) / numberOfTestsDone;
+    var VGpercentLabel = document.getElementById('procentVG');
+    VGpercentLabel.innerHTML = percentVG.toFixed(2) + '%';
+
+    var numberOfPasses = 0;
+    numberOfPasses = numberOfGs + numberOfVGs;
+
+    var passesLabel = document.getElementById('passes');
+    passesLabel.innerHTML = numberOfPasses;
+
+    var passesProcent = percentG + percentVG;
+    var ppLabel = document.getElementById('procentPasses');
+    ppLabel.innerHTML = passesProcent.toFixed(2) + '%';
+
+    var averagePoints = 0;
+    for(var i = 0 ; i<correctedTest.totalStudentPoints.length ; i++) {
+        if (correctedTest.testName[i] == chosenTest) {
+            averagePoints += correctedTest.totalStudentPoints[i][0];
+            //console.log(correctedTest.totalStudentPoints[i]);
+        }
+    }
+    averagePoints = (averagePoints / numberOfTestsDone).toFixed(2);
+
+    var averageLabel = document.getElementById('average');
+    averageLabel.innerHTML = averagePoints;
+
+
+
 }
 
